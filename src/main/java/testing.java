9c2dd9d9b0584e5bfdc7901f1920b3ee8a392664
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2022-09-27 01:20:02 +0200

    Delete testing.java

--- src/main/java/testing.java
+++
@@ -1,6 +0,0 @@
-public class testing {
-    public static void main(String[] args) {
-        String lot = "(U:Hideki Natsumi(316496607564529666) / G:botTest(1007571198261071902)), ";
-        System.out.println(lot.length());
-    }
-}
